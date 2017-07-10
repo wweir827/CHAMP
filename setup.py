3e0f4a8006284403b0de487513fa4cd9c0e85ff8
wweir827 <wweir827@gmail.com>
2017-07-10 08:08:29 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='champ',
+    version='1.0',
+    packages=['champ'],
+    url='http://github.com/wweir827/champ',
+    license='',
+    author='William Weir',
+    author_email='wweir@med.unc.edu',
+    description='Convex Hull of Admissible Modularity Partitions'
+)
