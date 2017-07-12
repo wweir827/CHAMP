1b6bb96496c5caa252221460664e5c538e6d2b2c
wweir827 <wweir827@gmail.com>
2017-07-12 14:32:45 -0400

    added mock imports for installing on RTD

--- conf.py
+++ conf.py
@@ -20,6 +20,18 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
+
+#Mock Modules Needed for Sphynx
+import mock
+
+MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot',
+                'matplotlib.cm',
+                'matplotlib.colors',
+                'matplotlib.patches',
+                'matplotlib.path']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
