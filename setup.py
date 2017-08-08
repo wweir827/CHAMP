0866db4a7b489ee6a247029d9874855f966453f1
wweir827 <wweir827@gmail.com>
2017-08-08 10:04:30 -0400

    cleaned up test files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 options=dict( name='champ',
-    version='1.0.4',
+    version='1.0.5',
     packages=['champ'],
     url='http://github.com/wweir827/champ',
     license='GPLv3+',
