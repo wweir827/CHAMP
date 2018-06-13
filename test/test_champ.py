2d6ee76ea16b7d8a83ee295c3941fac1d2b681e2
wweir827 <wweir827@gmail.com>
2018-06-13 13:51:59 -0400

    added test file for calling multilayer louvain

---
+++ test/test_champ.py
@@ -0,0 +1,227 @@
+from context import champ
+import igraph as ig
+import numpy as np
+import scipy.io as scio
+import pandas as pd
+import sys, os
+import numpy as np
+np.set_printoptions(precision=4, suppress=True)
+import matplotlib.pyplot as plt
+from time import time
+import modbp
+import forceatlas2 as fa2
+import seaborn as sbn
+import sklearn.metrics as skm
+import champ
+import louvain
+import scipy.io as scio
+
+
+def test_parallel_run():
+	G = ig.Graph.Erdos_Renyi(n=100, p=.04, directed=False)
+
+	G.es['weight']=np.random.normal(loc=10,scale=1,size=G.ecount())
+	partition_ensemble = champ.parallel_louvain(G,
+												start=.1,
+												fin=5.1,
+												numruns=10,
+												weight='weight',
+												progress=True)
+	print (partition_ensemble)
+
+
+def test_time_multilayer():
+
+
+	total_n=[1000,2000,4000,5000,10000]
+	nlayers_vals=[10,20,50]
+	n = 100
+	q = 4
+	nblocks = q
+	c = 4
+	ep = .1
+	eta = .1
+	pin = (n * c / (2.0)) / ((n / float(q)) * (n / float(q) - 1) / 2.0 * float(q) + ep * (q * (q - 1) / 2.0) * (
+		np.power(n / (q * 1.0), 2.0)))
+
+	pout = ep * pin
+	prob_mat = np.identity(nblocks) * pin + (np.ones((nblocks, nblocks)) - np.identity(nblocks)) * pout
+
+	times=pd.DataFrame(columns=['method','n','nlayers','totaln','time'])
+
+	for totn in total_n:
+
+		for nlayers in nlayers_vals:
+			n=totn/nlayers
+			print(n,nlayers)
+		
+			ml_sbm = modbp.MultilayerSBM(n, comm_prob_mat=prob_mat, layers=nlayers, transition_prob=eta)
+			mgraph = modbp.MultilayerGraph(intralayer_edges=ml_sbm.intraedges, interlayer_edges=ml_sbm.interedges,
+										   layer_vec=ml_sbm.layer_vec,
+										   comm_vec=ml_sbm.get_all_layers_block())
+
+			layers, interslice, g_full = champ.call_slices_to_layers_from_edge_list(intralayer_edges=mgraph.intralayer_edges,
+																					interlayer_edges=mgraph.interlayer_edges,
+																					layer_vec=mgraph.layer_vec)
+			gamma=1.0
+			omega=1
+			args = (layers, interslice, gamma, omega)
+			t=time()
+			part = champ.louvain_ext._parallel_run_louvain_multimodularity(args)
+			t=time()-t
+			cind=times.shape[0]
+			times.loc[cind,['method','n','nlayers','totaln','time']]='old',n,nlayers,totn,t
+
+			#run the new version
+			t=time()
+
+			intraslice, interslice = champ.create_multilayer_igraph_from_edgelist(intralayer_edges=mgraph.intralayer_edges,
+																				  interlayer_edges=mgraph.interlayer_edges,
+																				  layer_vec=mgraph.layer_vec)
+
+			RBCpartobj = louvain.RBConfigurationVertexPartitionWeightedLayers(intraslice, resolution_parameter=1.0,
+																			  layer_vec=mgraph.layer_vec.tolist())
+			InterlayerPartobj = louvain.RBConfigurationVertexPartition(interslice, resolution_parameter=0.0)
+
+			opt = louvain.Optimiser()
+			opt.optimise_partition_multiplex(partitions=[RBCpartobj, InterlayerPartobj])
+			t = time() - t
+			cind = times.shape[0]
+			times.loc[cind, ['method','n', 'nlayers', 'totaln', 'time']] ='new',n, nlayers, totn, t
+
+
+	plt.close()
+	# f,a=plt.subplots(1,1,figsize=(10,5))
+	sbn.lmplot(x='totaln',y='time',hue='nlayers',data=times,col='method',fit_reg=False)
+	plt.suptitle("Runtime Louvain on multilayer SBM")
+	plt.savefig('louvain_runtimes_multilayer_comparison.pdf')
+	plt.show()
+
+
+	# print(skm.adjusted_mutual_info_score(part[0]['partition'],mgraph.comm_vec))
+
+	# plt.close()
+	# a=plt.subplot2grid((1,2),(0,0))
+	# a.set_title('Ground')
+	# champ.plot_multilayer_community(mgraph.comm_vec,mgraph.layer_vec,ax=a)
+	# a=plt.subplot2grid((1,2),(0,1))
+	# a.set_title("Multilayer gamma={:.3f},omega={:.3f}".format(gamma,omega))
+	# champ.plot_multilayer_community(part[0]['partition'],mgraph.layer_vec,ax=a)
+	# plt.gcf().set_size_inches((10,5))
+	# plt.show()
+
+def test_multilayer_louvain():
+	n = 500
+	q = 2
+	nlayers = 2
+	nblocks = q
+	c = 8
+	ep = .1
+	eta = .1
+	pin = (n * c / (2.0)) / ((n / float(q)) * (n / float(q) - 1) / 2.0 * float(q) + ep * (q * (q - 1) / 2.0) * (
+		np.power(n / (q * 1.0), 2.0)))
+
+	pout = ep * pin
+
+	prob_mat = np.identity(nblocks) * pin + (np.ones((nblocks, nblocks)) - np.identity(nblocks)) * pout
+
+	ml_sbm = modbp.MultilayerSBM(n, comm_prob_mat=prob_mat, layers=nlayers, transition_prob=eta)
+	mgraph = modbp.MultilayerGraph(intralayer_edges=ml_sbm.intraedges, interlayer_edges=ml_sbm.interedges,
+								   layer_vec=ml_sbm.layer_vec,
+								   comm_vec=ml_sbm.get_all_layers_block())
+
+	intralayer, interlayer = champ.create_multilayer_igraph_from_edgelist(intralayer_edges=mgraph.intralayer_edges
+																			  ,
+																			  interlayer_edges=mgraph.interlayer_edges,
+
+																			  layer_vec=mgraph.layer_vec)
+	# gamma = 1.0
+	# omega = 1
+	# args = (intralayer,interlayer,mgraph.layer_vec, gamma, omega)
+	# part = champ.louvain_ext._parallel_run_louvain_multimodularity(args)
+	# # print(part[])
+	# print(part[0]['orig_mod'])
+	# print (skm.adjusted_mutual_info_score(mgraph.comm_vec,part[0]['partition']))
+	#test parallel version
+
+	ML_PartEnsemble=champ.louvain_ext.parallel_multilayer_louvain(intralayer_edges=mgraph.intralayer_edges,interlayer_edges=mgraph.interlayer_edges,layer_vec=mgraph.layer_vec,gamma_range=[0,1],ngamma=5,omega_range=[0,1],nomega=5,numprocesses=3,maxpt=(1,1))
+	print("Size of CHAMP: {:d} of {:d} runs".format(len(ML_PartEnsemble.ind2doms),ML_PartEnsemble.numparts))
+	print('number unique parititons : {:d}'.format(len(ML_PartEnsemble.get_unique_coeff_indices())))
+
+	filename=ML_PartEnsemble.save()
+	ML_PartEnsemble2=champ.PartitionEnsemble().open(filename)
+	ML_PartEnsemble2=ML_PartEnsemble.merge_ensemble(ML_PartEnsemble2,new=True)
+	ML_PartEnsemble2.save(filename='test2_partensemble.hdf5')
+
+	print("Size of CHAMP: {:d} of {:d} runs".format(len(ML_PartEnsemble2.ind2doms),ML_PartEnsemble2.numparts))
+	print('number unique parititons : {:d}'.format(len(ML_PartEnsemble2.get_unique_coeff_indices())))
+
+
+	print()
+	plt.close()
+	f,a=plt.subplots(1,2,figsize=(14,7))
+	a=plt.subplot(1,2,1)
+	a=ML_PartEnsemble.plot_2d_modularity_domains(ax=a)
+	a=plt.subplot(1,2,2)
+	a=ML_PartEnsemble2.plot_2d_modularity_domains(ax=a)
+
+	plt.show()
+
+
+def get_layer_average_ami(labs1,labs2,layer_vec):
+	layers,cnts=np.unique(layer_vec,return_counts=True)
+	allamis=[]
+	for i,layer in enumerate(layers):
+		cinds=np.where(layer_vec==layer)[0]
+		cami=skm.adjusted_mutual_info_score(labs1[cinds],labs2[cinds])
+		cami=cami*cnts[i]/(1.0*len(layer_vec))
+		allamis.append(cami)
+	return np.sum(allamis)
+
+	
+def test_on_senate_data():
+
+	senate_dir = '/Users/whweir/Documents/UNC_SOM_docs/Mucha_Lab/Mucha_Python/modularity_domains/multilayer_senate'
+	senate_data_file = os.path.join(senate_dir, 'multisenate0.5.mat')
+	sendata = scio.loadmat(senate_data_file)
+
+	A = sendata['A']
+	C = sendata['C']
+	sesid = sendata['Ssess'][:, 0]
+	parties = sendata['Sparty'][:, 0]
+	sessions = np.unique(sesid)
+	sess2layer = dict(zip(sessions, range(len(sessions))))
+	layer_vec = np.array(map(lambda x: sess2layer[x], sesid))
+	intralayer,interlayer=champ.create_multilayer_igraph_from_adjacency(A=A,C=C,layer_vec=layer_vec)
+
+	#test single run
+	gamma = .8
+	omega = 1.0
+	args = (intralayer, interlayer,layer_vec, gamma, omega)
+	# args=(champ.louvain_ext.adjacency_to_edges(A),
+	#	   champ.louvain_ext.adjacency_to_edges(C),
+	#	   layer_vec,1.0,.5)
+	part = champ.louvain_ext._parallel_run_louvain_multimodularity(args)
+	print(skm.adjusted_mutual_info_score(part[0]['partition'], parties))
+	print(get_layer_average_ami(part[0]['partition'],parties,layer_vec))
+	print()
+
+	#test parallel function
+
+
+	# plt.close()
+	# a = plt.subplot2grid((1, 2), (0, 0))
+	# a.set_title('Parties')
+	# champ.plot_multilayer_community(parties, layer_vec, ax=a)
+	# a = plt.subplot2grid((1, 2), (0, 1))
+	# a.set_title("Multilayer gamma={:.3f},omega={:.3f}".format(gamma, omega))
+	# champ.plot_multilayer_community(part[0]['partition'], layer_vec, ax=a)
+	# plt.gcf().set_size_inches((10, 5))
+	# plt.show()
+
+def main():
+	test_multilayer_louvain()
+	return 0
+
+if __name__=='__main__':
+	main()
