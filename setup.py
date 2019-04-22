38d3f824c6204d8a58643110aa544de5f141f9d3
wweir827 <wweir827@gmail.com>
2019-04-22 10:36:56 -0400

    fixed dependency for scipy to be >0.19 which is where the HalfspaceIntersection was added

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ options=dict( name='champ',
                  "Topic :: Scientific/Engineering :: Information Analysis",
                  ],
     install_requires=['ipython<5.9','matplotlib','future','numpy>1.13','h5py',
-                      'scipy','sklearn','louvain','python-igraph','seaborn','tqdm'],
+                      'scipy>=0.19','sklearn','louvain','python-igraph','seaborn','tqdm'],
     dependency_links=['https://github.com/wweir827/louvain-igraph/tarball/master#egg=louvain-igraph-0.6.1.champ']
 )
 #    install_requires=['pyhull','igraph','louvain','matplotlib','numpy',]
